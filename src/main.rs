d3006d015b300e91d40927b612d7b8b6ee917b43
clintonruairi <clintonruairi@gmail.com>
2021-12-15 14:23:27 +0400

    added mutable impl args to avoid ownership issues

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,6 @@ struct CubeSat {
 }
 
 #[derive(Debug)]
-enum StatusMessage {
-    Ok,
-}
-
-#[derive(Debug)]
 struct Mailbox {
     messages: Vec<Message>,
 }
@@ -20,26 +15,42 @@ type Message = String;
 
 struct GroundStation;
 
+impl GroundStation {
+    fn send(
+        &self,
+        to: &mut CubeSat,
+        msg: Message,
+    ) {
+        to.mailbox.messages.push(msg);
+    }
+}
 
-fn check_status(sat_id: CubeSat) -> CubeSat {
-    println!("{:?}: {:?}", sat_id, StatusMessage::Ok);
-    sat_id
+impl CubeSat {
+    fn recv(&mut self) -> Option<Message> {
+        self.mailbox.messages.pop()
+    }
 }
 
 fn main() {
-    let sat_a = CubeSat { id: 0 };
-    let sat_b = CubeSat { id: 1 };
-    let sat_c = CubeSat { id: 2 };
+    let base = GroundStation {};
+    let mut sat_a = CubeSat {
+        id: 0,
+        mailbox: Mailbox {
+            messages: vec![],
+        },
+    };
 
-    let sat_a = check_status(sat_a);
-    let sat_b = check_status(sat_b);
-    let sat_c = check_status(sat_c);
-    
-    let a_status = check_status(sat_a);
-    let b_status = check_status(sat_b);
-    let c_status = check_status(sat_c);
-    
+    println!("t0: {:?}", sat_a);
 
+    base.send(&mut sat_a, 
+             Message::from("hello there!"));
+
+    println!("t1: {:?}", sat_a);
+
+    let msg = sat_a.recv();
+    println!("t2: {:?}", sat_a);
+
+    println!("msg: {:?}", msg);
 }
 
 
